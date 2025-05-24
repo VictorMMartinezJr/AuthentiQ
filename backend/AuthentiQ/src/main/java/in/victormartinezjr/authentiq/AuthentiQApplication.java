3a37dbdbb8fec2ddd2cfb33a4135769757f110e8
Victor <victorm.martinezjr@outlook.com>
2025-05-24 18:03:24 -0400

    Movd .git to project root to track frontend and backend

---
+++ backend/AuthentiQ/src/main/java/in/victormartinezjr/authentiq/AuthentiQApplication.java
@@ -0,0 +1,13 @@
+package in.victormartinezjr.authentiq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthentiQApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthentiQApplication.class, args);
+    }
+
+}
---
+++ backend/AuthentiQ/src/test/java/in/victormartinezjr/authentiq/AuthentiQApplicationTests.java
@@ -0,0 +1,13 @@
+package in.victormartinezjr.authentiq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthentiQApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
