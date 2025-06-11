209d3cb5ed7e8fdb40b62922bf4d208ffb93da03
Victor <victorm.martinezjr@outlook.com>
2025-06-10 22:02:29 -0400

    create Header component and add to Home page

--- frontend/client/src/assets/assets.js
+++ frontend/client/src/assets/assets.js
@@ -1,5 +1,7 @@
 import logo from "./logo.png";
+import homepage_logo from "./homepage_logo.jpg";
 
 export const assets = {
   logo,
+  homepage_logo,
 };
--- frontend/client/src/pages/Home.jsx
+++ frontend/client/src/pages/Home.jsx
@@ -1,7 +1,13 @@
+import Header from "../components/Header";
 import MenuBar from "../components/Menubar";
 
 const Home = () => {
-  return <MenuBar />;
+  return (
+    <div className="flex flex-col items-center justify-content-center min-vh-100">
+      <MenuBar />
+      <Header />
+    </div>
+  );
 };
 
 export default Home;
